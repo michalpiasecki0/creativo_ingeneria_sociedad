494044db0ff260bb4e5776a72e3c2b9abfd174d3
michalpiasecki0 <michal.piasecki0@gmail.com>
2022-04-13 16:46:34 +0200

    all direction

--- app.py
+++ app.py
@@ -1,44 +1,45 @@
 import os
 
+import plotly.express as px
 import pandas as pd
 import numpy as np
 from dash import Dash
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import plotly.express as px
 import dash_core_components as dcc
 import dash_html_components as html
 
-print(os.getcwd())
-data = pd.read_csv("data/avocado.csv")
 transformed = pd.read_csv("data/transformed.csv")
-data["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
-data.sort_values("Date", inplace=True)
+transformed = transformed.loc[transformed['Year'] > 1994, :]
 
-external_stylesheets = [
-    {
-        "href": "https://fonts.googleapis.com/css2?"
-        "family=Lato:wght@400;700&display=swap",
-        "rel": "stylesheet",
-    },
-]
+n_colors = len(transformed.Country.unique())
+colorscale = colors = px.colors.sample_colorscale("viridis" ,[n/(n_colors -1) for n in range(n_colors)])
+colordict = {f:colorscale[i] for i, f in enumerate(transformed.Country.unique())}
 
 
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+quick_dictionary = dict()
+quick_dictionary['Internet'] = '% de habitantes usando Internet'
+quick_dictionary['Móvil'] = 'Número de moviles para 100 habitantes'
+quick_dictionary['Banda Ancha'] = '% de empresas usando el internet de la velocidad alta'
+
+
+
+app = Dash(__name__)
 server = app.server
 
-app.title = "La telecomunicacion!"
+app.title = "Creativo_piasecki"
 
 app.layout = html.Div(
     children=[
         html.Div(
             children=[
                 html.H1(
-                    children="Avocado Analytics", className="header-title"
+                    children="Desarollamiento de varios medios de telecomunicacíon", className="header-title"
                 ),
                 html.P(
-                    children="Analyze the behavior of avocado prices"
-                    " and the number of avocados sold in the US"
-                    " between 2015 and 2018",
+                    children="Este app muestra desarollamiento de diferentes aspectos de telecomunicacíon en varios paises. Todos"
+                             " datos son cogidos de World Bank Open Data. Adémas el grafo de GDP deja comparar el desarrolamietento de telecomunicacíón "
+                             "y el desarollamiento de economía",
                     className="header-description",
                 ),
             ],
@@ -48,53 +49,35 @@ app.layout = html.Div(
             children=[
                 html.Div(
                     children=[
-                        html.Div(children="Region", className="menu-title"),
+                        html.Div(children="País", className="menu-title"),
                         dcc.Dropdown(
                             id="region-filter",
                             options=[
                                 {"label": country, "value": country}
                                 for country in np.sort(transformed.Country.unique())
                             ],
-                            value="Albany",
+                            value="Spain",
                             clearable=False,
                             className="dropdown",
+                            multi=True
                         ),
                     ]
                 ),
                 html.Div(
                     children=[
-                        html.Div(children="Type", className="menu-title"),
+                        html.Div(children="Tipo de telecomunicacíón", className="menu-title"),
                         dcc.Dropdown(
                             id="type-filter",
                             options=[
-                                {"label": avocado_type, "value": avocado_type}
-                                for avocado_type in data.type.unique()
+                                {"label": telecomunnicacion_type, "value": telecomunnicacion_type}
+                                for telecomunnicacion_type in ["Móvil", "Internet", "Banda Ancha"]
                             ],
-                            value="organic",
+                            value="Internet",
                             clearable=False,
                             searchable=False,
                             className="dropdown",
                         ),
                     ],
-                ),
-                html.Div(
-                    children=[
-                        html.Div(
-                            children="Date Range",
-                            className="menu-title"
-                            ),
-                        dcc.Dropdown(
-                            id="date-range",
-                            options=[
-                                {"label": year, "value": year}
-                                for year in transformed.Year.unique()
-                            ],
-                            value="2000",
-                            clearable=False,
-                            searchable=False,
-                            className="dropdown",
-                        ),
-                    ]
                 ),
             ],
             className="menu",
@@ -121,57 +104,60 @@ app.layout = html.Div(
 
 
 @app.callback(
-    [Output("price-chart", "figure"), Output("volume-chart", "figure")],
+    [
+        Output("price-chart", "figure"),
+        Output("volume-chart", "figure")
+    ],
     [
         Input("region-filter", "value"),
-        #Input("type-filter", "value"),
-        Input("date-range", "year"),
+        Input("type-filter", "value"),
+        #Input("date-range", "year"),
         #Input("date-range", "end_date"),
     ],
 )
-def update_charts(country, year):
-    mask = (
-        (transformed.Country == country)
-    )
+
+
+
+def update_charts(country, telecommunication_type):
+
+    if type(country) == str:
+        mask = (
+            (transformed['Country'] == country)
+        )
+    else:
+        mask = (
+            (transformed['Country'].isin(country))
+        )
+
     filtered_data = transformed.loc[mask, :]
-    price_chart_figure = {
-        "data": [
-            {
-                "x": filtered_data["Year"],
-                "y": filtered_data["GDP per capita"],
-                "type": "lines",
-                "hovertemplate": "$%{y:.2f}<extra></extra>",
-            },
-        ],
-        "layout": {
-            "title": {
-                "text": "Average Price of Avocados",
-                "x": 0.05,
-                "xanchor": "left",
-            },
-            "xaxis": {"fixedrange": True},
-            "yaxis": {"tickprefix": "$", "fixedrange": True},
-            "colorway": ["#17B897"],
-        },
-    }
 
-    volume_chart_figure = {
-        "data": [
-            {
-                "x": filtered_data["Year"],
-                "y": filtered_data["Cellular per citizen"],
-                "type": "lines",
-            },
-        ],
-        "layout": {
-            "title": {"text": "Avocados Sold", "x": 0.05, "xanchor": "left"},
-            "xaxis": {"fixedrange": True},
-            "yaxis": {"fixedrange": True},
-            "colorway": ["#E12D39"],
-        },
-    }
+    price_chart_figure = px.line(
+        filtered_data,
+        x = 'Year',
+        y=telecommunication_type,
+        color='Country',
+        color_discrete_map=colordict,
+        title=quick_dictionary[telecommunication_type],
+        markers=True
+    )
+    price_chart_figure.update_layout(
+        {'paper_bgcolor': 'rgba(0,0,0,0)',
+         'plot_bgcolor': 'rgba(0,0,0,0)'},
+    )
+    volume_chart_figure = px.line(
+        filtered_data,
+        x='Year',
+        y='GDP per capita',
+        color='Country',
+        color_discrete_map=colordict,
+        title="GDP Per Capita",
+        markers=True
+    )
+    volume_chart_figure.update_layout(
+        {'paper_bgcolor': 'rgba(0,0,0,0)',
+         'plot_bgcolor': 'rgba(0,0,0,0)'},
+    )
+
     return price_chart_figure, volume_chart_figure
-
-
 if __name__ == "__main__":
     app.run_server(debug=True)
