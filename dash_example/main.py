4ca41ce9a86c722b28ea96d8def1f059f9ccbb3f
michalpiasecki0 <michal.piasecki0@gmail.com>
2022-04-13 12:10:18 +0200

    all direction

---
+++ dash_example/main.py
@@ -0,0 +1,177 @@
+import os
+
+import pandas as pd
+import numpy as np
+from dash import Dash
+from dash.dependencies import Input, Output
+import plotly.express as px
+import dash_core_components as dcc
+import dash_html_components as html
+
+print(os.getcwd())
+data = pd.read_csv("C:\\Users\\micha\\OneDrive\\Pulpit\\studia\\UAM\\ingeneria_sociedad\\creativo\\creativo_ingeneria_sociedad\\dash_example\\data\\avocado.csv")
+transformed = pd.read_csv("C:\\Users\\micha\\OneDrive\\Pulpit\\studia\\UAM\\ingeneria_sociedad\\creativo\\creativo_ingeneria_sociedad\\dash_example\\data\\transformed.csv")
+data["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
+data.sort_values("Date", inplace=True)
+
+external_stylesheets = [
+    {
+        "href": "https://fonts.googleapis.com/css2?"
+        "family=Lato:wght@400;700&display=swap",
+        "rel": "stylesheet",
+    },
+]
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+
+app.title = "La telecomunicacion!"
+
+app.layout = html.Div(
+    children=[
+        html.Div(
+            children=[
+                html.P(children="📡", className="header-emoji"),
+                html.H1(
+                    children="Avocado Analytics", className="header-title"
+                ),
+                html.P(
+                    children="Analyze the behavior of avocado prices"
+                    " and the number of avocados sold in the US"
+                    " between 2015 and 2018",
+                    className="header-description",
+                ),
+            ],
+            className="header",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.Div(children="Region", className="menu-title"),
+                        dcc.Dropdown(
+                            id="region-filter",
+                            options=[
+                                {"label": country, "value": country}
+                                for country in np.sort(transformed.Country.unique())
+                            ],
+                            value="Albany",
+                            clearable=False,
+                            className="dropdown",
+                        ),
+                    ]
+                ),
+                html.Div(
+                    children=[
+                        html.Div(children="Type", className="menu-title"),
+                        dcc.Dropdown(
+                            id="type-filter",
+                            options=[
+                                {"label": avocado_type, "value": avocado_type}
+                                for avocado_type in data.type.unique()
+                            ],
+                            value="organic",
+                            clearable=False,
+                            searchable=False,
+                            className="dropdown",
+                        ),
+                    ],
+                ),
+                html.Div(
+                    children=[
+                        html.Div(
+                            children="Date Range",
+                            className="menu-title"
+                            ),
+                        dcc.Dropdown(
+                            id="date-range",
+                            options=[
+                                {"label": year, "value": year}
+                                for year in transformed.Year.unique()
+                            ],
+                            value="2000",
+                            clearable=False,
+                            searchable=False,
+                            className="dropdown",
+                        ),
+                    ]
+                ),
+            ],
+            className="menu",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="price-chart", config={"displayModeBar": False},
+                    ),
+                    className="card",
+                ),
+                html.Div(
+                    children=dcc.Graph(
+                        id="volume-chart", config={"displayModeBar": False},
+                    ),
+                    className="card",
+                ),
+            ],
+            className="wrapper",
+        ),
+    ]
+)
+
+
+@app.callback(
+    [Output("price-chart", "figure"), Output("volume-chart", "figure")],
+    [
+        Input("region-filter", "value"),
+        #Input("type-filter", "value"),
+        Input("date-range", "year"),
+        #Input("date-range", "end_date"),
+    ],
+)
+def update_charts(country, year):
+    mask = (
+        (transformed.Country == country)
+    )
+    filtered_data = transformed.loc[mask, :]
+    price_chart_figure = {
+        "data": [
+            {
+                "x": filtered_data["Year"],
+                "y": filtered_data["GDP per capita"],
+                "type": "lines",
+                "hovertemplate": "$%{y:.2f}<extra></extra>",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "Average Price of Avocados",
+                "x": 0.05,
+                "xanchor": "left",
+            },
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"tickprefix": "$", "fixedrange": True},
+            "colorway": ["#17B897"],
+        },
+    }
+
+    volume_chart_figure = {
+        "data": [
+            {
+                "x": filtered_data["Year"],
+                "y": filtered_data["Cellular per citizen"],
+                "type": "lines",
+            },
+        ],
+        "layout": {
+            "title": {"text": "Avocados Sold", "x": 0.05, "xanchor": "left"},
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"fixedrange": True},
+            "colorway": ["#E12D39"],
+        },
+    }
+    return price_chart_figure, volume_chart_figure
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
